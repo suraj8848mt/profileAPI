00d22451b68d04ce811c28c759b4268c870745e9
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-02-16 15:38:11 +0545

    created a user profile manager

--- src/profile_project/profile_api/models.py
+++ src/profile_project/profile_api/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
+
+
+
+class UserProfileManager(BaseUserManager):
+	'''
+	Helps django work with our custom user model
+	'''
+	def create_user(self, email, name, password=None):
+		''' Creates a new user profile object '''
+		if not email:
+			raise ValueError('User Must have an Email Address')
+		email = self.normalize_email(email)
+		user = self.model(email=email, name=name)
+		user.set_password(password)
+		user.save(using=self._db)
+
+		return user
+
+
+	def create_super_user(self, email,name, password):
+		''' creates and saves a new superuser with given details '''
+		user = self.create_user(email, name, password)
+		user.is_superuser = True
+		user.is_staff = True
+
+		user.save(using=self._db)
+
 
 
 class UserProfile(models.Model):
