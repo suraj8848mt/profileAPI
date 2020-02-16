e9de29d888167e12885b9fb17464d6147c441ca9
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-02-16 21:39:44 +0545

    created UserProfileSerializer.

--- src/profile_project/profile_api/serializer.py
+++ src/profile_project/profile_api/serializer.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
 	''' Seriallizes a name fields for testing our APIView '''
 	name = serializers.CharField(max_length=20)	
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+	''' A serializer for our user profile objects '''
+
+	class Meta:
+		model = models.UserProfile
+		fields = ('id', 'email', 'name', 'password')
+		extra_kwargs = {'password': {'write_only': True}}
+
+
+	def create(self, validated_data):
+		''' Create and retur a new user '''
+
+		user = model.UserProfile(
+			email=validated_data['emai'],
+			name = validated_data['name']
+			)
+		user.set_password(validated_data['password'])
+		user.save()
+
+		return user
+
